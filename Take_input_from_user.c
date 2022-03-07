0812f6e9b69a8775795c5c9d45661bb1a36ec9cb
samir07dev <samir07ali@gmail.com>
2022-03-07 18:21:52 +0530

    practise question

---
+++ Take_input_from_user.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+
+    int number;
+    printf("Enter the number of a cube:\n");
+    scanf("%d",&number);
+    printf("the cube of a number is : %d",number*number*number);
+    return 0;
+}
