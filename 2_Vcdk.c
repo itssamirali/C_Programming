480fba8dd4fad6cfbc6586325deed1d121948d8d
samir07dev <samir07ali@gmail.com>
2022-03-07 17:43:02 +0530

    Variable, Data_types-Constant_Keywords

---
+++ 2_Vcdk.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+
+    int number = 20;
+    float pi = 3.14;
+    char star = '*';
+
+    printf("value of number is %d \n",number);
+    printf("value of pi is %f \n",pi);
+    printf("value of star is %c \n",star);
+    return 0;
+
+}
