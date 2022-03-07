d014da4b003df6210c6ff4c7123b80c9bdd75bdf
samir07dev <samir07ali@gmail.com>
2022-03-07 18:21:34 +0530

    practise question

---
+++ Program.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+
+    //Initializing a and b
+    int a; int b;
+    printf("enter the value of a and b: ");
+    scanf("%d %d",&a,&b);
+    /*Calculate the sum of a and b
+            a + b */
+    printf("The sum of a and b is %d",a+b);
+}
