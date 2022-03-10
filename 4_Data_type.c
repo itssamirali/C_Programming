04f8f80fbcae1a165a9b5ed1881c2fca047fde6f
samir07dev <samir07ali@gmail.com>
2022-03-10 22:37:04 +0530

    data_type

---
+++ 4_Data_type.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+
+    /*int age = 56;
+    int old_age = age;
+    int new_age = old_age + 5;
+    printf("New age is : %d",new_age);*/
+
+    int rupee = 5, dollar;
+    dollar = 8;
+    printf("The ruppee is : %d \n",rupee);
+    printf("The dollar is : %d",dollar);
+}
+
