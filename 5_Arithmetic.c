b2df45fad11b0e5bb54e7e2620c6f37162602da2
samir07dev <samir07ali@gmail.com>
2022-03-10 22:43:12 +0530

    Arithmetic_Operation

---
+++ 5_Arithmetic.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+
+    int a = 2, b = 5, c = 9;
+    /*int sum = a+b-a*b/a-b+c;
+    printf("The value is : %d",sum);
+    a = b + c;
+    printf("%d",a);*/
+
+    printf("%d\n",a+b+c);
+    printf("%d\n",a-b);
+    printf("%d\n",a*b*c);
+    printf("%d\n",a/b);
+    printf("%d\n",a%b);
+}
