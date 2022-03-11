dd6b965a3d977730c6bf6869b7c7da778fd95f6d
samir07dev <samir07ali@gmail.com>
2022-03-12 01:06:13 +0530

    Increment/Decrement

---
+++ 5a_Incre_Decre.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main(){
+
+    // int x,y;
+    // printf("Enter the value of x and y :\n");
+    // scanf("%d%d",&x,&y);
+    // printf("%d\n%d\n",x,y,++x);
+    // printf("%d",x);
+    // return 0;
+
+    // int a = 5;
+    // a++;     //post increment
+    // printf("%d\n",a);
+    // int b;
+    // b = a + 2;
+    // ++a;  //pre increment
+    // printf("%d\n%d",a,b);
+
+    
+    int a = 5;
+    a--;     //post decrement
+    printf("%d\n",a);
+    int b;
+    b = a + 2;
+    --a;  //pre decrement
+    printf("%d\n%d",a,b);
+
+
+
+
+
+
+
+}
