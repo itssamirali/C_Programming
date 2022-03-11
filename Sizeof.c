b2981c057ce191dda221336c8c989d45493f970d
samir07dev <samir07ali@gmail.com>
2022-03-12 01:16:21 +0530

    size_of

---
+++ Sizeof.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+
+    int a;
+    float b;
+    char c;
+    double d;
+    long int l;
+    printf("%d\n",sizeof(a));
+    printf("%d\n",sizeof(b));
+    printf("%d\n",sizeof(c));
+    printf("%d\n",sizeof(d));
+    printf("%d",sizeof(l));
+}
