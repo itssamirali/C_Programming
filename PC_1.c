ec02ddb710b17ee08478b9c617efa472a0445e26
samir07dev <samir07ali@gmail.com>
2022-09-12 10:23:40 +0530

    Practise Problem 1

---
+++ PC_1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+    printf("******* Here is my Details *******\n");
+    printf("Name          :     Samir Ali\n");
+    printf("Date of Birth :     07th Dec\n");
+    printf("Mobile No     :     8271157125\n");
+}
