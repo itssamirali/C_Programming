dc80e68430d42a520054038972d7b0ce8e12109d
samir07dev <samir07ali@gmail.com>
2022-09-13 21:15:56 +0530

    practise problem 3

---
+++ PC_3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+
+    int height = 7;
+    int wight = 5;
+    int area = height * wight;
+    int perimeter = 2 * height + wight;
+
+    printf("The area of a Rectangle is : %d\n", area);
+    printf("The perimeter of a rectangle is : %d\n", perimeter);
+}
