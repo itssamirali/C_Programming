b92bac1afb73119e5da29010c85362649360c656
samir07dev <samir07ali@gmail.com>
2022-09-13 21:05:55 +0530

    Pratise Problem 2

---
+++ PC_2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+
+    char a1 = 'S';
+    char a2 = 'A';
+    char a3 = 'M';
+    char a4 = 'I';
+    char a5 = 'R';
+
+    printf("The Reverse of %c%c%c%c%c is %c%c%c%c%c\n", a1, a2, a3, a4, a5, a5, a4, a3, a2, a1);
+}
