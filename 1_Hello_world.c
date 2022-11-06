fd77d0e517b368ff535b5285725edc68c534afd0
samir07dev <samir07ali@gmail.com>
2022-11-06 08:28:36 +0530

    code change

--- 1_Hello_world.c
+++ 1_Hello_world.c
@@ -1,5 +1,11 @@
+/*
+    Author: "Samir Ali"
+    Course: "C Programming"
+    Topic:  "Welcome Code"
+*/
 #include <stdio.h>
 int main()
 {
-    printf("Hello to the world of C Programming");
+    puts("Welcome to the Journey of C Programming");
+    printf("I am here to learn C programming");
 }
