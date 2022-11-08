376edbf23b587ccd36a73b11f252d0cf87ef3d55
samir07dev <samir07ali@gmail.com>
2022-11-08 22:50:22 +0530

    extension change

--- Area_of_circle.c
+++ Area_of_circle.c
@@ -6,8 +6,8 @@ int main()
     const float PI = 3.14;
     float radius;
 
-    printf("Enter the radius of circle :");
-    scanf("%f", &radius);
+    printf("Enter the radius of circle : ");
+    getchar("%f", &radius);
     float area = 2 * PI * radius;
     printf("The Area of a Circle is : %.1f", area);
 }
---
+++ add.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int a, b, sum;
+    printf("Enter the value of a: \n");
+    scanf("%d", &a);
+    printf("Enter the value of b: \n");
+    scanf("%d", &b);
+    sum = a + b;
+    printf("The addition of a and b is : %d", sum);
+}
