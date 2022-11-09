52214f233458b80f8a4f8af7392c4fc17af76040
samir07dev <samir07ali@gmail.com>
2022-11-10 00:21:21 +0530

    Type Casting

---
+++ typeconversion.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int a = 98;
+    float b = a;
+    double c = 8.5855;
+    int d = (int)c;
+    char e = 'H';
+    int f = e;
+    char g = a;
+
+    printf("The value of b is %f\n", b);
+    printf("The value of d is %d\n", d);
+    printf("The value of d is %c\n", g);
+}
