eca4d3856b8cc5de64f70685fd2d967da4270bb9
samir07dev <samir07ali@gmail.com>
2022-11-17 21:54:52 +0530

    daily practise 6

---
+++ Daily_Practise/DP_6.c
@@ -0,0 +1,20 @@
+// Write a C program to find whether a given year is a leap year or not...
+
+#include <stdio.h>
+
+int main()
+{
+
+    int year;
+    printf("Enter the year : ");
+    scanf("%d", &year);
+
+    if (year % 4 == 0)
+    {
+        printf("%d is a leap year", year);
+    }
+    else
+    {
+        printf("%d is not a leap year", year);
+    }
+}
