fcd1aa8edb3ea015eec9fa8cceaa36dad165dcc2
samir07dev <samir07ali@gmail.com>
2022-11-17 22:05:11 +0530

    daily practise 7

---
+++ Daily_Practise/DP_7.c
@@ -0,0 +1,17 @@
+// Write a program in C to display the first 10 natural numbers...
+
+#include <stdio.h>
+
+int main()
+{
+
+    int n = 10;
+
+    printf("The 10 natural numbers are : ");
+
+    for (int i = 1; i <= 10; i++)
+    {
+
+        printf("%d ", i);
+    }
+}
