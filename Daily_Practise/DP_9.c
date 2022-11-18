16878a522f78c59b00d31018dc9b0f326bd3b254
samir07dev <samir07ali@gmail.com>
2022-11-18 21:51:17 +0530

    daily practise 9

---
+++ Daily_Practise/DP_9.c
@@ -0,0 +1,19 @@
+// Write a program in C to display n terms of natural number and their sum...
+
+#include <stdio.h>
+
+int main()
+{
+
+    int num, sum = 0;
+    puts("Enter a number : ");
+    gets(num);
+    printf("The nth term of natural numbers are : ");
+    for (int i = 1; i < n; i++)
+    {
+
+        printf("%d ", i)
+            sum = sum + 1;
+    }
+    printf("\nThe sum of nth natural mnumbers are : %d", sum);
+}
