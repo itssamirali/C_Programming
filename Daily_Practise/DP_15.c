d242c45100cb1bedaf7fa81f46d790ad38fc9d78
samir07dev <samir07ali@gmail.com>
2022-12-05 12:08:15 +0530

    Static Variable

--- Daily_Practise/DP_15.c
+++ Daily_Practise/DP_15.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
+int b = 5, c = 7; // Global Variable
 int main()
+
 {
 
-    int a = 5; // local variable
-    printf("The value of a is : %d", a);
+    int a = 5;        // local variable
+    static int x = 5; // static variable
+    printf("The value of a is : %d\n", a);
+    printf("The sum of a and b is : %d\n", a + b);
+    printf("The value of c is : %d\n", c);
+    printf("The value of x is : %d\n", x);
+    x = a + x;
+    printf("The sum of a and x is : %d", a + x);
 }
