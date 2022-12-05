1e613a3cff3c47006370620a4c017c22170fb286
samir07dev <samir07ali@gmail.com>
2022-12-05 12:17:08 +0530

    Auto variable

--- Daily_Practise/DP_15.c
+++ Daily_Practise/DP_15.c
@@ -6,10 +6,14 @@ int main()
 
     int a = 5;        // local variable
     static int x = 5; // static variable
+    auto int m;
+    int n;
     printf("The value of a is : %d\n", a);
     printf("The sum of a and b is : %d\n", a + b);
     printf("The value of c is : %d\n", c);
     printf("The value of x is : %d\n", x);
     x = a + x;
-    printf("The sum of a and x is : %d", a + x);
+    printf("The sum of a and x is : %d\n", a + x);
+    printf("%d\n", m);
+    printf("%d", n);
 }
